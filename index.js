c98e4fca72f9f5bd8d167b428a5009e3972bf09a
Alexander Eric <70110009+alexandereric995@users.noreply.github.com>
2021-05-03 13:24:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+/*!
+* Edit By Alexander Eric@Eric Lapin
+* MG Brand Test
+*/
+
+// DEMO: Tooltip on
+function furcanTooltip(tooltip) {
+  $('body > .tooltip').remove();
+  $(tooltip).tooltip({
+    trigger: 'hover',
+    container: 'body',
+  });
+};
+furcanTooltip('[data-toggle="tooltip"]');
+
+$(document).on('click', function () {
+  if ($('body > .tooltip').length > 0) {
+    $('body > .tooltip').remove();
+  }
+});
+// DEMO: Tooltip off
+
+// DEMO: Turkish Keys on
+var turkishKeyboard = [
+  {
+    "0": "Q",
+    "1": "W",
+    "2": "E",
+    "3": "R",
+    "4": "T",
+    "5": "Y",
+    "6": "U",
+    "7": "I",
+    "8": "O",
+    "9": "P",
+    "10": "Ğ",
+    "11": "Ü"
+  },
+  {
+    "0": "A",
+    "1": "S",
+    "2": "D",
+    "3": "F",
+    "4": "G",
+    "5": "H",
+    "6": "J",
+    "7": "K",
+    "8": "L",
+    "9": "Ş",
+    "10": "İ",
+  },
+  {
+    "0": "Z",
+    "1": "X",
+    "2": "C",
+    "3": "V",
+    "4": "B",
+    "5": "N",
+    "6": "M",
+    "7": "Ö",
+    "8": "Ç"
+  }
+];
+// DEMO: Turkish Keys off
+
+// DEMO: KioskBoard Init off
+KioskBoard.Init({
+  // keysJsonUrl: 'kioskboard-keys-turkish.json',
+  keysArrayOfObjects: turkishKeyboard,
+  language: 'tr',
+  keysFontFamily: 'Barlow',
+  keysFontWeight: '500',
+  cssAnimations: true,
+  // allowRealKeyboard: false,
+  // allowMobileKeyboard: false,
+});
+KioskBoard.Run('.virtual-keyboard-demo');
+// DEMO: KioskBoard Init off
